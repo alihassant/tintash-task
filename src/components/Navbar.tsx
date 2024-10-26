12fabf40bd29ce63c4582ec12541b9ac4809dd08
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-10-27 01:18:45 +0500

    fix navbar responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <header className="fixed left-0 top-0 z-50 w-full bg-[#213488] bg-opacity-60 p-4 text-white backdrop-blur-[6px]">
-      <div className="container mx-auto flex flex-col md:flex-row md:items-center md:px-20">
+      <div className="container mx-auto flex flex-col md:flex-row md:items-center xl:px-20">
         <div className="flex w-full items-center justify-between md:w-3/5">
           <Link href="/" className="text-2xl font-bold">
             Tintash
@@ -23,7 +23,7 @@ const Navbar = () => {
         </div>
 
         <nav
-          className={`mx-auto mt-4 w-full md:mt-0 md:w-2/5 ${
+          className={`mx-auto mt-4 w-full md:mt-0 lg:w-2/5 ${
             menuOpen ? "block" : "hidden"
           } flex flex-col items-center justify-center space-y-2 md:flex md:flex-row md:justify-between md:space-y-0`}
         >
